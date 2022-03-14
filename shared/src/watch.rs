03eece2c73f3cc7cd86c80b068ec2611ce0f73f6
tami5 <kkharji@protonmail.com>
2022-03-14 03:20:48 +0300

    feat(core): support watching renames and fix duplicated events

--- shared/src/watch.rs
+++ shared/src/watch.rs
@@ -1,8 +1,10 @@
 use crate::state::SharedState;
 use crate::Command;
-use notify::{RecursiveMode, Watcher};
+use notify::{Error, Event, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::Path;
-use tokio::sync::mpsc;
+use std::result::Result;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
 use tracing::{debug, info, trace};
 use wax::{Glob, Pattern};
 
@@ -32,49 +34,79 @@ pub async fn update(state: SharedState, _msg: Command) {
     }
 }
 
+/// HACK: ignore seen paths.
+///
+/// Sometiems we get event for the same path, particularly
+/// `ModifyKind::Name::Any` is ommited twice for the new path
+/// and once for the old path.
+///
+/// This will compare last_seen with path, updates `last_seen` if not match,
+/// else returns true.
+async fn should_ignore(last_seen: Arc<Mutex<String>>, path: &str) -> bool {
+    let path = path.to_string();
+    let mut last_seen = last_seen.lock().await;
+    if last_seen.to_string() == path {
+        return true;
+    } else {
+        *last_seen = path;
+        return false;
+    }
+}
+
 fn new(state: SharedState, root: String) -> tokio::task::JoinHandle<anyhow::Result<()>> {
     // NOTE: should watch for registerd directories?
     let ignore = wax::any::<Glob, _>(["**/.git/**", "**/*.xcodeproj/**", "**/.*"]).unwrap();
 
     tokio::spawn(async move {
         let (tx, mut rx) = mpsc::channel(100);
-        // FIXME: Sometimes (more then one/duplicated) event is processed at the same time
-        let mut watcher = notify::recommended_watcher(
-            move |res: std::result::Result<notify::Event, notify::Error>| {
-                if res.is_ok() {
-                    tx.blocking_send(res.unwrap()).unwrap()
-                };
-            },
-        )?;
+
+        let mut watcher = RecommendedWatcher::new(move |res: Result<Event, Error>| {
+            if res.is_ok() {
+                tx.blocking_send(res.unwrap()).unwrap()
+            };
+        })?;
 
         watcher.watch(Path::new(&root), RecursiveMode::Recursive)?;
-        watcher.configure(notify::Config::PreciseEvents(true))?;
 
-        info!("[New::{:?}]", &root);
+        // HACK: ignore seen paths.
+        let last_seen = Arc::new(Mutex::new(String::default()));
 
         while let Some(event) = rx.recv().await {
             let state = state.clone();
             let path = match event.paths.get(0) {
-                Some(p) => p.clone(),
+                Some(p) => match p.clone().to_str() {
+                    Some(s) => s.to_string(),
+                    None => continue,
+                },
                 None => continue,
             };
 
-            if ignore.is_match(path.to_str().unwrap()) {
+            if should_ignore(last_seen.clone(), &path).await || ignore.is_match(&*path) {
                 continue;
             }
 
             // NOTE: maybe better handle in tokio::spawn?
-            match event.kind {
+            match &event.kind {
                 notify::EventKind::Create(_) => {
                     debug!("[FileCreated]: {:?}", path);
                 }
                 notify::EventKind::Remove(_) => {
                     debug!("[FileRemoved]: {:?}", path);
                 }
+                notify::EventKind::Modify(m) => match m {
+                    notify::event::ModifyKind::Name(_) => {
+                        // HACK: only account for new path
+                        if !Path::new(&path).exists() {
+                            continue;
+                        }
+                        debug!("[FileRenamed]: NewName: {:?}", path);
+                    }
+                    _ => continue,
+                },
                 _ => continue,
             }
 
-            trace!("[NewEvent] {:#?}", event);
+            trace!("[NewEvent] {:#?}", &event);
 
             let state = state.lock().await;
             let _workspace = match state.workspaces.get(&root) {
