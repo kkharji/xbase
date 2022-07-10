2d293e2e92b2a19afa4a270c8f045493dd60659f
kkharji <kkharji@protonmail.com>
2022-07-10 18:23:07 +0300

    feat(typescript): handle server messages

--- vscode/src/index.ts
+++ vscode/src/index.ts
@@ -1,6 +1,46 @@
-import { commands, window } from 'vscode'
+import type { ExtensionContext } from 'vscode'
+import { commands, window, workspace } from 'vscode'
+import * as util from './util'
+import XBaseServer from './server'
 
-export function activate() {
+let server: XBaseServer
+
+export async function activate(_context: ExtensionContext) {
+  console.debug('Activating XBase')
+  const connect = await XBaseServer.connect()
+
+  let root: string
+
+  if (workspace.workspaceFolders && (workspace.workspaceFolders.length > 0)) {
+    root = workspace.workspaceFolders[0].uri.fsPath
+  }
+  else if (workspace.workspaceFolders) {
+    root = workspace.workspaceFolders[0].uri.fsPath
+    console.warn(`Found multiple roots, using ${root}`)
+  }
+  else {
+    console.error('No Workspace Opened, ignoring ..')
+    return
+  }
+
+  if (connect.isErr()) {
+    console.error(`Fail to activate XBase: ${connect.unwrapErr()}`)
+    return
+  }
+  else { server = connect.unwrap() }
+
+  // TODO: Setup Editor Status
+  const register = await server.register(root)
+  if (register.isErr()) {
+    console.error(`Fail to register workspaceFolder: ${register.unwrapErr()}`)
+    return
+  }
+  else {
+    const name = util.nameFromPath(root)
+    window.showInformationMessage(`[${name}] Registered`)
+  }
+
+  // TODO: Move commands to commands.ts
   commands.registerCommand('xbase.run', () => {
     window.showInformationMessage('Pick target/scheme to run ...')
   })
@@ -12,8 +52,7 @@ export function activate() {
   })
 }
 
-export function deactivate() {
+export function deactivate(_context: ExtensionContext) {
   // TODO: Drop all roots
-  // TODO: Close connection
-
+  server.socket.end()
 }
