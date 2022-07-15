8780d834747ddc54ada537d7ac8e4da205a7991d
kkharji <kkharji@protonmail.com>
2022-07-15 17:20:36 +0300

    feat(vscode): append errors from build log to problems

--- vscode/xbase/ui/logger.ts
+++ vscode/xbase/ui/logger.ts
@@ -1,19 +1,15 @@
-import vscode from "vscode";
-import { window } from "vscode";
+import vscode, { DiagnosticSeverity, Position, Range, Uri } from "vscode";
 import { ContentLevel } from "../types";
 
+
+
 export default class Logger implements vscode.Disposable {
-  private channel: vscode.OutputChannel;
+  private channel = vscode.window.createOutputChannel("XBase", "xclog");
+  public diagnosticCollection = vscode.languages.createDiagnosticCollection("XBase");
+  private problemRegex = /\[(.*)\] (.*):(\d+):(\d+):(?:\s(.*):)?\s+(.*)$/;
+
   // TODO: find a more accurate way to check whether output window is shown
   private shown = false;
-
-  constructor() {
-    this.channel = window.createOutputChannel("XBase", "xclog");
-  }
-
-  dispose() {
-    this.channel.dispose();
-  }
 
   /* show output */
   public async toggle() {
@@ -28,15 +24,51 @@ export default class Logger implements vscode.Disposable {
     }
   }
 
-  // TODO: output source code warnings & errors to Problems
   append(line: string, level: ContentLevel) {
-    // TODO: find out based on vscode current log level
-    this.channel.appendLine(line);
-    switch (level) {
-      case "Error": console.error(line); break;
-      case "Warn": console.warn(line); break;
-      case "Debug": console.debug(line); break;
-      case "Info": console.info(line); break;
+    const groups = this.problemRegex.exec(line);
+
+    if (groups) {
+      const filePath = Uri.file(groups[2]);
+      const lineNr = parseInt(groups[3]);
+      const colNr = parseInt(groups[4]);
+      const type = groups[5] ? groups[5] : groups[1].toLowerCase();
+      const message = groups[6];
+      const currentDiagnostics = this.diagnosticCollection.get(filePath);
+      const diagnostics = [...(currentDiagnostics ? currentDiagnostics : [])];
+
+      let severity;
+      if (type === "error")
+        severity = DiagnosticSeverity.Error;
+      else if (type === "warn" || type === "warning")
+        severity = DiagnosticSeverity.Warning;
+      else
+        severity = DiagnosticSeverity.Information;
+
+      // TODO: Ensure that this message doesn't already exists in problems!
+      diagnostics.push({
+        message,
+        range: new Range(
+          new Position(lineNr, colNr),
+          new Position(lineNr, colNr + 10)
+        ),
+        severity,
+      });
+
+      this.diagnosticCollection.set(filePath, diagnostics);
+
+    } else if (/\[(Error|Warning)\]/.test(line) === false) {
+      this.channel.appendLine(line);
+      switch (level) {
+        case "Error": console.error(line); break;
+        case "Warn": console.warn(line); break;
+        case "Debug": console.debug(line); break;
+        case "Info": console.info(line); break;
+      }
     }
+  }
+
+  dispose() {
+    this.channel.dispose();
+    this.diagnosticCollection.dispose();
   }
 }
