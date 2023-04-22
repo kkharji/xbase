kkharji/xbase

commit 26a99a1534054cedb6dcca9b6c8331d11d4cf8dc

fix(sourcekit-helper): use uri to file path (#173)
* fix(sourcekit-helper) Use uri.to_file_path()

This fixes issues when trying to get compile arguments
for file paths where there is a space. The URI is percent
escaping but the files in the .compile file are not.

* feat(sourcekit-helper) Add log if init fails

crates/sourcekit-helper/src/main.rs
@@ -119,7 +119,9 @@ fn register_for_changes(conn: &Conn, id: Id, params: OptionsChangedRequest) -> R
         return Ok(());
     }
 
-    let filepath = params.uri.path();
+    let filepath = params.uri
+        .to_file_path()
+        .map_err(|_| anyhow!("Invalid File URI: {:?}", params.uri))?;
     // tracing::info!("{filepath}");
     let root_path = state().lock().unwrap().root_path.clone();
     let uri = Url::from_directory_path(root_path).ok();
@@ -252,7 +254,15 @@ fn main() -> Result<()> {
 
     let (conn, io_threads) = Connection::stdio();
     tracing::info!("Started");
-    conn.initialize(|params| initialize(&params).expect("Initialize"))?;
+    conn.initialize(|params|
+        match initialize(&params) {
+            Err(err) => {
+                tracing::error!("Failed to Initialize: ${}", err);
+                panic!("Initialization Failure: ${}", err);
+            },
+            Ok(build) => build
+        }
+    )?;
     tracing::info!("Initialized");
 
     for msg in &conn.receiver {

commit 4134c6322c60e82ca6c6e7fc3dc65665d92913bb

chore(fmt): fix cargo fmt warnings
caused by #173, the PR should've been rejected until resolve that
warning.

crates/sourcekit-helper/src/main.rs
@@ -119,7 +119,8 @@ fn register_for_changes(conn: &Conn, id: Id, params: OptionsChangedRequest) -> R
         return Ok(());
     }
 
-    let filepath = params.uri
+    let filepath = params
+        .uri
         .to_file_path()
         .map_err(|_| anyhow!("Invalid File URI: {:?}", params.uri))?;
     // tracing::info!("{filepath}");
@@ -254,15 +255,13 @@ fn main() -> Result<()> {
 
     let (conn, io_threads) = Connection::stdio();
     tracing::info!("Started");
-    conn.initialize(|params|
-        match initialize(&params) {
-            Err(err) => {
-                tracing::error!("Failed to Initialize: ${}", err);
-                panic!("Initialization Failure: ${}", err);
-            },
-            Ok(build) => build
+    conn.initialize(|params| match initialize(&params) {
+        Err(err) => {
+            tracing::error!("Failed to Initialize: ${}", err);
+            panic!("Initialization Failure: ${}", err);
         }
-    )?;
+        Ok(build) => build,
+    })?;
     tracing::info!("Initialized");
 
     for msg in &conn.receiver {