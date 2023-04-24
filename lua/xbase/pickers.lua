9d69ae34c130844cf27e6a9534db52225b7ca08b
kkharji <kkharji@protonmail.com>
2023-04-24 13:50:17 +0300

    ref(neovim): make telescope optional as picker

--- lua/xbase/pickers.lua
+++ lua/xbase/pickers.lua
@@ -1,3 +1,11 @@
-local telescope = require "xbase.pickers.telescope"
+--Types
 
-return telescope
+---@class XBaseSelectOptions
+---@field root string? the project root to run against (default current root)
+
+local ok, _ = pcall(require, "telescope")
+if ok then
+  return require "xbase.pickers.telescope"
+else
+  return require "xbase.pickers.builtin"
+end
